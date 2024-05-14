42639b6957f85fdc5e1368662c652d6b73c5311e
martinaliporata <martinaliporata@gmail.com>
2024-05-14 07:34:37 +0200

    aggiunto const per userMail

---
+++ Mail/js/script.js
@@ -0,0 +1,12 @@
+// Mail
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+// Creare un prompt per chiedere all'utente la mail.
+// Creare un array per verificare che il suo nome sia all'interno della lista delle persone che possono accedere
+// Inserisci un console log per dare all'utente un feedback.
+
+const userMail = prompt('type your mail')
+console.log(userMail)
+
