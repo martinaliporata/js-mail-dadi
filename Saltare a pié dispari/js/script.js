fad3c18a9db66ee201a9e64befe52162d125a3ee
martinaliporata <martinaliporata@gmail.com>
2024-05-14 07:53:11 +0200

    inserito array vuoto

---
+++ Saltare a pié dispari/js/script.js
@@ -0,0 +1,8 @@
+// Saltare a piè dispari [Bonus]
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è pari inseriscilo nell’array
+
+// Crea un array vuoto
+// Con un ciclo for chiedi all'utente di inserire un numero
+// Inserisci con un'istruzione condizionale (if) la possibilità di inserire un numero in array solo se è pari.
+
+const array = []
